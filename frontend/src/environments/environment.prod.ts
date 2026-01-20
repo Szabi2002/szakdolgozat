6d7f3d208025aa1c1a644cd3447db3e686cf2ac9
Nagynemes Szabolcs <h162202@stud.u-szeged.hu>
2026-01-20 22:22:18 +0100

    Deploy bugfix 156

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://szakdolgozat-uc7f.onrender.com',
+  apiUrl: 'https://szakdolgozat-uc7f.onrender.com/api',
   supabase: {
     url: 'https://prhlsuwkokuisqavwfoi.supabase.co',
     anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InByaGxzdXdrb2t1aXNxYXZ3Zm9pIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjIwOTM4NzAsImV4cCI6MjA3NzY2OTg3MH0.YCq2SR1djQiCUNqoI27ZIHBT8Vrka6HcPmm1ryQUpEk',
