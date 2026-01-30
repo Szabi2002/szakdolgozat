b9da529f2e92bbddcd1840454ab2394be539c0f7
Nagynemes Szabolcs <h162202@stud.u-szeged.hu>
2026-01-30 19:41:35 +0100

    Backend jav

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://legislative-ginni-szakdolgozat-daff4e98.koyeb.app',
+  apiUrl: 'https://legislative-ginni-szakdolgozat-daff4e98.koyeb.app/api',
   supabase: {
     url: 'https://prhlsuwkokuisqavwfoi.supabase.co',
     anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InByaGxzdXdrb2t1aXNxYXZ3Zm9pIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjIwOTM4NzAsImV4cCI6MjA3NzY2OTg3MH0.YCq2SR1djQiCUNqoI27ZIHBT8Vrka6HcPmm1ryQUpEk',
